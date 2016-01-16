70624c190007fd3e6756b6e9f38fc4e06723e665
WebFreak001 <janju007@web.de>
2016-01-16 14:18:27 +0100

    Added cancelable events

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 # Tiny Event System
 
-Its just 10 lines of code (including unittests)!
+Its just 20 lines of code and it supports regular events & cancelable events!
 
 Usage:
 
-```D
+```d
 import tinyevent;
+
+// Regular event
 Event!string onStringChange;
 onStringChange ~= (str) { /* Handle new string */ };
 onStringChange.emit("Foo");
+```
+
+```d
+import tinyevent;
+
+// Cancelable
+Cancelable!bool onQuit;
+onQuit ~= (force) { return force || !saved; }
+
+// When pressing X:
+if(!onQuit.emit(false))
+	showUnsavedChangesDialog();
+else
+	exit();
 ```
--- source/tinyevent.d
+++ source/tinyevent.d
@@ -1,14 +1,42 @@
 module tinyevent;
 
+/// Defines a regular event
 alias Event(Args...) = void delegate(Args)[];
+/// Defines a cancelable event by returning false to cancel
+alias Cancelable(Args...) = bool delegate(Args)[];
 
+/// Calls all functions in a regular event
 void emit(Args...)(void delegate(Args)[] events, Args args) {
 	foreach(fn; events)
 		fn(args);
 }
 
+/// Calls all functions in a cancelable event
+bool emit(Args...)(bool delegate(Args)[] events, Args args) {
+	foreach(fn; events)
+		if(!fn(args))
+			return false;
+	return true;
+}
+
+///
 unittest {
 	Event!string onStringChange;
 	onStringChange ~= (s) { assert(s == "Foo"); };
 	onStringChange.emit("Foo");
+}
+
+///
+unittest {
+	Cancelable!int onIntChange;
+	int changed = 0;
+	onIntChange ~= (i) { if(i > 5) return false; changed++; return true; };
+	onIntChange ~= (i) { if(i > 4) return false; changed++; return true; };
+	onIntChange ~= (i) { if(i > 3) return false; changed++; return true; };
+	assert(onIntChange.emit(2));
+	assert(changed == 3);
+	
+	changed = 0;
+	assert(!onIntChange.emit(4));
+	assert(changed == 2);
 }
